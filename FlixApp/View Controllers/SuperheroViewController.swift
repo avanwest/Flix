9feb4d88310372aa90ac6be392f8daa31c95d7a5
Adam vanWestrienen <avanwest@gmail.com>
2018-09-17 12:21:56 -0400

    setup collection view

---
+++ FlixApp/View Controllers/SuperheroViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SuperheroViewController.swift
+//  FlixApp
+//
+//  Created by Adam vanWestrienen on 9/17/18.
+//  Copyright © 2018 Adam vanWestrienen. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies: [[String:Any]] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.dataSource = self
+        fetchMovies()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let movie = movies[indexPath.item]
+        if let posterPathString = movie["poster_path"] as? String {
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURLString + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+        }
+        return cell
+    }
+    
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=901e3582797c5ce2354e05e2543fd98b")!
+        let request = URLRequest(url: url, cachePolicy: . reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+//                self.refreshControl.endRefreshing()
+            }
+        }
+        task.resume()
+        
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FlixApp/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  FlixApp
+//
+//  Created by Adam vanWestrienen on 9/17/18.
+//  Copyright © 2018 Adam vanWestrienen. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
